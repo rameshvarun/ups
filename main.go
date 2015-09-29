b1ac685929e9b6036f9e503df00ac3017eeb6ab5
Varun Ramesh <varunramesh@live.com>
2015-09-29 02:48:58 -0700

    Can now parse and emit the Mother3 translation ups.

--- main.go
+++ main.go
@@ -4,16 +4,20 @@ import (
 	"io/ioutil"
 
 	"github.com/rameshvarun/ups/reader"
+	"github.com/rameshvarun/ups/writer"
 )
 
 func main() {
-	data, err := ioutil.ReadFile("test.ups")
+	data, err := ioutil.ReadFile("mother3.ups")
 	if err != nil {
 		panic(err)
 	}
 
-	_, err = reader.ReadUPS(data)
+	patch, err := reader.ReadUPS(data)
 	if err != nil {
 		panic(err)
 	}
+
+	newdata := writer.WriteUPS(patch)
+	ioutil.WriteFile("mother3.out", newdata, 0644)
 }
--- writer/writer.go
+++ writer/writer.go
@@ -1,5 +1,41 @@
 package writer
 
+import (
+	"bytes"
+	"encoding/binary"
+	"hash/crc32"
+
+	"github.com/rameshvarun/ups/common"
+)
+
+// WriteUPS writes UPS data to a byte array.
+func WriteUPS(data *common.PatchData) []byte {
+	var buffer bytes.Buffer
+
+	// Write signature.
+	buffer.Write([]byte{'U', 'P', 'S', '1'})
+
+	// Write input and output file sizes.
+	buffer.Write(WriteVariableLengthInteger(data.InputFileSize))
+	buffer.Write(WriteVariableLengthInteger(data.OutputFileSize))
+
+	for _, block := range data.PatchBlocks {
+		buffer.Write(WriteVariableLengthInteger(block.RelativeOffset))
+		buffer.Write(block.Data)
+		buffer.WriteByte(0)
+	}
+
+	// Write the input and output checksums.
+	binary.Write(&buffer, binary.LittleEndian, data.InputChecksum)
+	binary.Write(&buffer, binary.LittleEndian, data.OutputChecksum)
+
+	// Checksum the buffer so far.
+	checksum := crc32.ChecksumIEEE(buffer.Bytes())
+	binary.Write(&buffer, binary.LittleEndian, checksum)
+
+	return buffer.Bytes()
+}
+
 // WriteVariableLengthInteger writes a variable-length encoded integer.
 // based off of the source for http://www.romhacking.net/utilities/606/
 func WriteVariableLengthInteger(value uint64) []byte {
