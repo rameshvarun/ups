bcb534bf03fc0540ff96e31fce656689ef1d044b
Varun Ramesh <varunramesh@live.com>
2015-09-29 02:24:04 -0700

    Working writer

--- writer/writer.go
+++ writer/writer.go
@@ -1,6 +1,18 @@
 package writer
 
 // WriteVariableLengthInteger writes a variable-length encoded integer.
-func WriteVariableLengthInteger(uint64) []byte {
-	return []byte{}
+// based off of the source for http://www.romhacking.net/utilities/606/
+func WriteVariableLengthInteger(value uint64) []byte {
+	var data []byte
+	x := value & 0x7f
+	value >>= 7
+
+	for value != 0 {
+		data = append(data, byte(x))
+		value--
+		x = value & 0x7f
+		value >>= 7
+	}
+	data = append(data, byte(0x80|x))
+	return data
 }
